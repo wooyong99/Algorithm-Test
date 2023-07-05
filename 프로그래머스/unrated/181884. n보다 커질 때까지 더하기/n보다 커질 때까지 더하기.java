8cdd097d4ec227a29b07e2f90e8ed39b4f778737
Woo Yong <85385921+wooyong99@users.noreply.github.com>
2023-07-05 19:55:43 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for(int num : numbers){
+            if(answer <= n){
+                answer += num;
+            }
+        }
+        return answer;
+    }
+}
