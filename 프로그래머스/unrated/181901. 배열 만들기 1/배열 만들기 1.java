2e7aab77b1cc33a3ad7fedd4257a094e79377a0a
Woo Yong <85385921+wooyong99@users.noreply.github.com>
2023-07-07 16:18:19 +0900

    [unrated] Title: 배열 만들기 1, Time: 5.38 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n, int k) {
+        return IntStream.rangeClosed(1,n)
+            .filter(e -> e%k==0)
+            .sorted()
+            .toArray();
+    }
+}
