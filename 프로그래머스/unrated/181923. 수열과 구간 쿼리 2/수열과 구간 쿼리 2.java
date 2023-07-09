a5bf5cadf4df4b2fdddc5e08d5f04412d6d15063
Woo Yong <85385921+wooyong99@users.noreply.github.com>
2023-07-09 18:50:57 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 11.88 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,28 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        List<Integer> rs = new ArrayList<>();
-        for(int[] querie : queries){
-            int s = querie[0];
-            int e = querie[1];
-            int k = querie[2];
-            List<Integer> list = new ArrayList<>();
-            for(int i = s ; i<=e; i++){
-                if(arr[i] > k){
-                    list.add(arr[i]);
-                }
-            }
-            if(list.isEmpty()){
-                rs.add(-1);
-            }else{
-                Collections.sort(list);
-                rs.add(list.get(0));
-            }
-        }
-        return rs.stream().mapToInt(e->(int)e).toArray();
+        return Stream.of(queries)
+            .map(e -> IntStream.rangeClosed(e[0],e[1])
+                 .map(a -> arr[a])
+                 .filter(k -> e[2] < k)
+                 .min()
+                )
+            .mapToInt(e -> e.orElse(-1))
+            .toArray();
     }
 }
