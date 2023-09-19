3b71f69d2d844dcebd1cb7915b47604aeeae6c8b
Woo Yong <85385921+wooyong99@users.noreply.github.com>
2023-09-19 17:51:08 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-        
-        for(int i = 0; i<a.length; i++){
-            answer += (a[i] * b[i]);
+        int sum = 0;
+        for(int i = 0 ; i<a.length; i++){
+            int num = a[i] * b[i];
+            sum += num;
         }
-        
-        return answer;
+        return sum;
     }
 }
